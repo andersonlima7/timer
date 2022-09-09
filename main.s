a029b74fb0e46376154d16ddf9ca0f09b34b34a0
Anderson Lima <andersonlll007@gmail.com>
2022-09-09 19:10:00 -0300

    Flashing leds

--- main.s
+++ main.s
@@ -4,59 +4,31 @@
 
 .global _start
 
-_start: 
-        mapMem @ mapemaento
-
-        @ Definicao dos pinos como entradas - Teste
-        GPIODirectionIn pin4
-        GPIODirectionIn pin5
-        GPIODirectionIn pin17
-        GPIODirectionIn pin19
-        GPIODirectionIn pin22
-        GPIODirectionIn pin26
-        GPIODirectionIn pin27
+_start:
+        mapMem @ mapemaento f
 
         @ Definicao dos pinos como saidas
-        GPIODirectionOut pin1
+        GPIODirectionOut pin6
         GPIODirectionOut pin12
         GPIODirectionOut pin16
         GPIODirectionOut pin20
         GPIODirectionOut pin21
         GPIODirectionOut pin25
-        
+
         @ variavel do while loop
         mov r1, #0
-        loop: 
-            @CMP r1, #0
-            @BNE loopdone            @ condicao de parada
-            @GPIOReadRegister pin5   @ ler o pino 5
-            @B loop                  @ continua o loop
+        loop:
             nanoSleep
-            GPIOTurnOn pin12
+            GPIOTurnOff pin6
             nanoSleep
-            GPIOTurnOn pin16
+            GPIOTurnOn pin6
             add r1, #1
-            CMP, r1, #10
+            CMP r1, #10
             BLE loop
-
-        loopdone:
-            @GPIOTurnOn pin12    @ se quebrar o loop, ligue o led no pino12
-            @mov r0, #1          @stdout - escrita no terminal
-            @ldr r1, =worked 
-            @mov r2, #20         @tamanho da frase
-            @mov R7, #4          @ funcao de escrever no linux
-	        @svc 0               @ da o controle pro linux chamar a funcao
-        
 
 
 _end:
     mov R0, #0 @ Use 0 return code
     mov R7, #1 @ Command code 1 terms
     svc 0 @ Linux command to terminate
-
-.data worked: .ascii "Botao pressionado! \n"
-
-
-
-
 
